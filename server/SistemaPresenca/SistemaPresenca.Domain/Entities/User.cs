5d9c7725440fc92ca3e817918775d919936140b4
Miguel Totti <migueltotti2005@gmail.com>
2026-05-12 13:10:50 -0300

    refactor: update user password property to be nullable

--- server/SistemaPresenca/SistemaPresenca.Domain/Entities/User.cs
+++ server/SistemaPresenca/SistemaPresenca.Domain/Entities/User.cs
@@ -6,7 +6,7 @@ public class User : BaseEntity
 {
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
     public string RegistrationId { get; set; }
     public string? Cpf { get; set; }
     public string? TagId { get; set; }
