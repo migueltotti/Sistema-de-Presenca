49f62a5d253fbb146010588cd33f13ae10e9379d
Miguel Totti <migueltotti2005@gmail.com>
2026-05-12 18:06:41 -0300

    feat: register database in program.cs and update appsettings

--- server/SistemaPresenca/SistemaPresenca.Api/Program.cs
+++ server/SistemaPresenca/SistemaPresenca.Api/Program.cs
@@ -1,7 +1,16 @@
+using SistemaPresenca.CrossCutting.Models;
+using SistemaPresenca.CrossCutting.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+var applicationSettings = builder.Configuration.GetApplicationSettings(builder.Environment);
+
+builder.Services
+    .AddSingleton<ISettings>(applicationSettings)
+    .AddControllers();
+
+builder.Services
+    .AddDatabase(applicationSettings.PostgresSettings);
 
 var app = builder.Build();
 
