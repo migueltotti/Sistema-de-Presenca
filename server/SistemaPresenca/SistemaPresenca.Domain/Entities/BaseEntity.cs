71292045c204f58f6e078543b0ebca497cd473e9
Miguel Totti <migueltotti2005@gmail.com>
2026-05-12 12:10:58 -0300

    refactor: change BaseEntity to abstracts class

--- server/SistemaPresenca/SistemaPresenca.Domain/Entities/BaseEntity.cs
+++ server/SistemaPresenca/SistemaPresenca.Domain/Entities/BaseEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaPresenca.Domain.Entities;
 
-public class BaseEntity
+public abstract class BaseEntity
 {
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
