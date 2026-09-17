f9a852f52710f842b2bd5b5cec9feec9202cef5e
Miguel Totti <migueltotti2005@gmail.com>
2026-09-17 18:04:04 -0300

    feat: add request model validation for POST /subject

---
+++ server/SistemaPresenca/SistemaPresenca.Application/Validators/Subjects/CreateSubjectRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using SistemaPresenca.Application.Requests.Subjects;
+
+namespace SistemaPresenca.Application.Validators.Subjects;
+
+public class CreateSubjectRequestValidator : AbstractValidator<CreateSubjectRequest>
+{
+    public CreateSubjectRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(200)
+            .WithMessage("Name is required and must not exceed 200 characters.");
+
+        RuleFor(x => x.Code)
+            .NotEmpty()
+            .MaximumLength(10)
+            .WithMessage("Code is required and must not exceed 10 characters.");
+
+        RuleFor(x => x.TotalClasses)
+            .GreaterThan(0)
+            .WithMessage("Total classes is required and must be a positive number.");
+    }
+}
